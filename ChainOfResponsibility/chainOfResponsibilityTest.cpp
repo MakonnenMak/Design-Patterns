2d845b4a1fc61718399a7e5c1f4f96301b00be02
mmako1 <solmak11@gmail.com>
2019-03-11 00:57:09 -0400

    Chain of responsibility complete

--- ChainOfResponsibility/chainOfResponsibilityTest.cpp
+++ ChainOfResponsibility/chainOfResponsibilityTest.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<string>
 #include "ATM.h"
-
+using namespace std;
 int main(){
 	ATM * a=new twentyDeposit();
 	ATM * b=new fiftyDeposit();
@@ -10,5 +10,6 @@ int main(){
 	a->setNextChain(b);
 	b->setNextChain(c);
 
-	a->deposit(20);
+	cout<<a->deposit(20)<<endl;
+	cout<<b->deposit(50)<<endl;
 }
